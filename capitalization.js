5af3640f59f253d9bd56805b17afc8c04609cd82
Lucas <lucaszago.ph@gmail.com>
2022-08-29 15:39:10 +0200

    TCW finish second capitalization

--- capitalization.js
+++ capitalization.js
@@ -1,7 +1,18 @@
+/*
+        3cApitalization
+*/
+//  const name = "LUCas";
+
+// let newName = name.toLowerCase();
+
+// newName = newName.replace(newName[2], newName[2].toUpperCase());
+
+// console.log(newName);
+
 const name = "LUCas";
 
 let newName = name.toLowerCase();
 
-newName = newName.replace(newName[2], newName[2].toUpperCase());
+newName = newName[0].toUpperCase() + newName.slice(1);
 
 console.log(newName);
