c4459c131dc2993c9fa66ad317e155f077213e9c
Lucas <lucaszago.ph@gmail.com>
2022-09-01 20:54:16 +0200

    TCW add colors, pause motion, and display count

--- bar-chart/Controller.js
+++ bar-chart/Controller.js
@@ -1,4 +1,5 @@
 "use strict";
+let clicked = false;
 function init() {
   for (let i = 0; i < 40; i++) {
     data.push(getCustomersQty());
@@ -10,10 +11,28 @@ function init() {
 }
 
 function loop() {
-  data.push(getCustomersQty());
-  data.shift();
-  updateUI(data);
+  if (!clicked) {
+    data.push(getCustomersQty());
+    data.shift();
+    updateUI(data);
+  }
   setTimeout(loop, 1000);
 }
 
+function addEvent(node, customers) {
+  node.addEventListener("mouseover", () => {
+    // console.log(node);
+    displayData(node, customers);
+  });
+  node.addEventListener("mouseout", () => {
+    // console.log(node);
+    displayData(node, customers, false);
+  });
+}
+document.body.addEventListener("mousedown", () => {
+  clicked = true;
+});
+document.body.addEventListener("mouseup", () => {
+  clicked = false;
+});
 document.addEventListener("DOMContentLoaded", init);
--- bar-chart/View.js
+++ bar-chart/View.js
@@ -1,13 +1,29 @@
 "use strict";
 const ul = document.querySelector("#bars");
+const h2 = document.querySelector("h2");
 
 function updateUI(entries) {
   ul.innerHTML = "";
   for (let i = 0; i < entries.length; i++) {
     let instance = document.createElement("li");
     instance.classList.add("bar");
+    instance.style.backgroundColor = `rgb(180, ${Math.floor(255 / 40) * i}, 120)`;
     instance.style.height = (ul.offsetHeight / 32) * entries[i] + "px";
+    addEvent(instance, entries[i]);
     // instance.style.width = ul.offsetWidth / 40;
     ul.insertAdjacentElement("beforeend", instance);
+  }
+}
+
+function displayData(node, customers, bool = true) {
+  // console.log(node.offsetHeight);
+  node.style.backgroundColor = "inherit";
+  node.classList.add("highlight");
+
+  h2.textContent = `Customers: ${customers}`;
+
+  if (!bool) {
+    updateUI(data);
+    h2.textContent = `Customers: `;
   }
 }
