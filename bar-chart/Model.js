3aedffb3791886e2fb335dd225497472ae680b3e
Lucas <lucaszago.ph@gmail.com>
2022-09-02 19:54:03 +0200

    add animals exercise

--- bar-chart/Model.js
+++ bar-chart/Model.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const data = [];
-for (let i = 0; i < 40; i++) {
-  data.push(getCustomersQty());
-}
+// for (let i = 0; i < 40; i++) {
+//   data.push(getCustomersQty());
+// }
 
 function getCustomersQty() {
   // Changed to 33 since it uses floor, as 32 it rounds down to 31;
